b5df71b7c86a4ecf60b8c6704c6529dd07a8a13a
vkulageri <vkulageri@VKULAGERIX88KU0.lenovo.com>
2019-02-26 23:28:42 +0530

    Maventest

---
+++ Maventest/src/mavendemo/MavenTest.java
@@ -0,0 +1,18 @@
+package mavendemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	
+	@Test
+	public void testA() {
+		WebDriver driver = new ChromeDriver();
+		driver.close();
+	}
+
+}
